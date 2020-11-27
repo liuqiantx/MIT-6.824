ececc38a152e5d114b7242025430f3882652063f
liuqiantx <liuqiantx1221@163.com>
2020-11-27 18:38:43 +0800

    add notes

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -80,18 +80,20 @@ type Raft struct {
 	me        int                 // this peer's index into peers[]
 	dead      int32               // set by Kill()
 
+	state     int         // 服务所处的状态，领导者，候选人或是下属
+
 	// 需持久化的
-	currentTerm int
-	votedFor    int
-	log         []*LogEntry
+	currentTerm int         //
+	votedFor    int         //
+	log         []*LogEntry //
 
 	// leader 容易丢失的
-	matchIndex []int
-	nextIndex  []int
+	matchIndex []int // 领导者所维护的对各个下属服务的下一个待匹配索引 (init lastLogIndex + 1)
+	nextIndex  []int // 领导者所维护的与各个下属服务匹配上的最高索引 (init 0),各个 follower 间如何区分？按在 peers 中的顺序？
 
 	// 所有 server 都容易丢失的
-	commitIndex int
-	lastApplied int
+	commitIndex int // 最近的一次已提交的日志的索引
+	lastApplied int // 最近一次已应用的日志的索引
 }
 
 // return currentTerm and whether this server
@@ -217,15 +219,21 @@ func (rf *Raft) killed() bool {
 // the struct itself.
 //
 type RequestVoteArgs struct {
-	//
+	term         int // 候选者所处的任期
+	candidateId  int // 候选者的 id
+	lastLogIndex int // 候选者的最新日志的索引,用于资格核验
+	lastLogTerm  int // 候选者的最新日志的所处任期,用于资格核验
 }
 
 type RequestVoteReply struct {
-	//
+	term        int  // follower 所处的任期,用于让候选者更新自己的任期及状态
+	voteGranted bool // follower 是否投票给 candidate
 }
 
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
-	// Your code here (2A, 2B).
+	// 当候选者的 term < 我的 currentTerm 时，拒绝
+	// 当我还未投票给任何人时 (votedFor is null)，或者我已经投给了该候选者时，当候选者的日志未落后于我的，我会给他投一票
+	// 当候选者的 term > 我的 currentTerm 时, 会将 currentTerm 转换为候选者的 term
 }
 
 func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
@@ -248,19 +256,41 @@ func (rf *Raft) isElectionTimeout() bool {
 }
 
 func (rf *Raft) startLeaderElection() {
-	// 长流程
+	// 我会先开启新一轮任期
+	// 给自己投票
+	// 开启新一轮选举阶段，重置选举结束倒计时(倒计时随机 150 ~ 300)
+	// 给 cluster 中所有团队成员发送 requestVote rpc
+	// 当收到超过一半团队成员关于我担任指定任期领导者的投票时,我的状态改为 leader.
+	// 当出现网络分区时,同一时期可能出现几个领导,但各个领导的任期不一样
+	// 每个 server 同一任期最多投一次票,以谁先来,先投谁的原则
 }
 
 func (rf *Raft) runLeader() {
 	// election 时 leader 要做的事
+	// 一旦当上领导人，马上发送 appendEntries rpc 给团队中的所有成员(同一区域内的),且会定期发送,避免任期过期,定期如何设定,需要注意???
+	// 当从客户那里收到命令时,先将命令日志添加到本地,只有在命令应用到状态机后,才给客户回复(并不是日志提交就给回复)
+	// 当 last log index (到底是哪个?) >= nextIndex (为 followers 记录的),给该 follower 发送 sendAppendEntries rpc,由 nextIndex 开始,即 AppendLogEntriesArgs 中的 preLogIndex 为 nextIndex -1
+	// 若成功了,则更新 leader raft 中为该 follower 维护的 nextIndex & matchIndex
+	// 若失败了,则降低 nextIndex,然后再次 sendAppendEntries rpc,至于 nextIndex 如何降低,是有捷径可走的. --> ?
+	// 需要对 commitIndex 做核对,看是否是对的值,核对方法:当存在一个 log 的 Index > 我记录的 commitIndex,然后该 index 所处的任期为我当前的任期,并且大部分下属们匹配上的 index >= N,基于日志一定单调递增的原则, 此时需要将 commitIndex = N -> 什么情况下会是这样的
+	// 当 leader 发现存在 term 比自己大的情况时,会将自己的状态改成 follower
 }
 
 func (rf *Raft) runCandidate() {
 	// 注意都需要有休息时间
+	// 一旦状态转换为 candidate，立马开始选举
+	// 一旦收到超 1/2 的团队投票,则将自己的状态转变为领导人.
+	// 当收到新的 安好信息，且该信息的发送者的任期不低于自己,立马将自己的状态转变为 follower.(在等待投票的过程中)
+	// 若安好信息发送者的任期低于自己的,他会拒绝信息,并且继续投票.
+	// 当选举倒计时结束时，开启新一轮选举
 }
 
 func (rf *Raft) runFollower() {
-	//
+	// raft 状态由 follower 开始,
+	// 除非从 candidate / leader 处收到非法的信息,或是超时都没收到信息,否则它会一直保持 follower 的状态
+	//　仅会给 candidate & leader 回复
+	// 每次投票完会重置选举倒计时，当选举倒计时结束，未收到领导的安好信息，或是其他人的投票信息，我会将自己的状态转换为 candidate，然后开始新一轮选举
+	// 当 heartBeatInternal 结束或者 electionTimeout 到来, follower 会将自己的状态改为 candidate
 }
 
 func (rf *Raft) Run() {
@@ -271,7 +301,9 @@ func (rf *Raft) Run() {
 // 关于 log replication
 //
 type LogEntry struct {
-	//
+	term  int // log 发布时所处的时期
+	Command  interface{}  // 任务详情
+	CommandIndex  int  // 任务索引
 }
 
 type ApplyMsg struct {
@@ -281,16 +313,27 @@ type ApplyMsg struct {
 }
 
 type AppendLogEntriesArgs struct {
-	//
+	term         int         // 领导者的任期
+	leaderId     int         // 当客户发错消息给下属时,下属可以告诉客户领导是谁
+	preLogIndex  int         // 前一日志的索引,用于资格核验
+	preLogTerm   int         // 前一日志所处的任期,用于资格核验
+	entries      []*LogEntry // 要添加的日志信息,为了效率,可能一次添加多条
+	leaderCommit int         // 领导通知提交的日志索引
 }
 
 type AppendLogEntriesReply struct {
-	//
+	term    int  // 告知领导我目前所处的日志,以便领导更新自己,然后领导转变状态
+	success bool // 当下属前一日志与领导相符时,回复成功
 }
 
 func (rf *Raft) AppendLogEntries(args *AppendLogEntriesArgs, reply *AppendLogEntriesReply) {
 	// 超长的添加流程
-}
+	// 当领导 term < 我的 currentTerm 时,拒绝
+	// 当前一日志所处的任期及索引与领导不匹配时,拒绝
+	// 当存在日志冲突时,删除该日志以及其后的所有日志.(索引相同,但所处的任期不同)
+	// 添加所有领导有,但我没有的日志(保证顺序一致)
+	// 若 leaderCommit > 我的 commitIndex, 则将 commitIndex = min(leaderCommit, 最新日志的索引) ,之后会随着我的日志更新, commitIndex 增大,直到跟上领导的进程
+	// 当领导的 term > 我的 currentTerm 时, 会将 currentTerm 转换为领导的 term
 
 func (rf *Raft) sendAppendLogEntries(server int, args *AppendLogEntriesArgs, reply *AppendLogEntriesReply) bool {
 	//
@@ -299,6 +342,7 @@ func (rf *Raft) sendAppendLogEntries(server int, args *AppendLogEntriesArgs, rep
 
 func (rf *Raft) SendApplyMsg() {
 	// 发送 log 中的未应用的 msgs
+	// 当我 commitIndex > lastApplied, 增加 lastApplied，然后在状态机上应用该条日志.
 }
 
 //
